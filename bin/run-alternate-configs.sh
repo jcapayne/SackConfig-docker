4cdfee6b5d2557130b5a0d594015a01c3d722049
John Payne <john@sackheads.org>
2019-08-14 21:26:44 -0400

    more change to try and get alternate configs working

--- bin/run-alternate-configs.sh
+++ bin/run-alternate-configs.sh
@@ -3,5 +3,13 @@
 cd /SackConfig
 for f in sack.conf.d/*.conf
 do
+  LOCALNAME=$(grep ^localname /SackConfig/${f} | awk '{ print $NF }')
+  echo "Localname is $LOCALNAME"
+  LOCALCONF=$(echo $LOCALNAME | sed 's/\.nameserver.net/.conf/')
+  if [ ! -e /SackConfig/$LOCALCONF ]; then
+    echo "Linking $LOCALCONF from webdir"
+    ln -s /usr/share/nginx/html/$LOCALCONF /SackConfig/$LOCALCONF
+  fi
+
   perl -I. ./sackfigurator ${f}
 done
